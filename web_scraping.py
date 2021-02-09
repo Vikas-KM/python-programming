9b233c81092bcc6a895cb3d703283c8196a9a6b5
Vikas KM <vikki.km@gmail.com>
2021-02-10 00:47:24 +0530

    webscraping the literacy data from wiki

---
+++ web_scraping.py
@@ -0,0 +1,24 @@
+# https://en.wikipedia.org/wiki/List_of_Indian_states_and_union_territories_by_literacy_rate
+# https://en.wikipedia.org/wiki/List_of_states_and_union_territories_of_India_by_crime_rate
+# https://en.wikipedia.org/wiki/Indian_states_ranking_by_availability_of_toilets
+# Web Scraping Example using the Literacy rates in India
+
+# BeautifulSoup Docs - https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
+# Loading the required libraries
+from bs4 import BeautifulSoup as bs
+import requests
+
+
+r = requests.get('https://en.wikipedia.org/wiki/List_of_Indian_states_and_union_territories_by_literacy_rate')
+
+soup = bs(r.content)
+
+# print(soup.title)
+print(soup.title.string)
+# print(soup.prettify())
+
+tables = soup.find_all('table', attrs={'class':'wikitable'})
+for table in tables:
+    print(table)
+    print('--'*30)
